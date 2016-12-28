ce8420ed126c27576e3ad9781fca1252641d8bb5
Dilip <dilip.arumugam@gmail.com>
2016-12-28 00:14:20 -0800

    Reordered fastest to slowest

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python data_curve.py L0 L0 5
-python data_curve.py L1 L1 5
 python data_curve.py L2 L2 5
+python data_curve.py L1 L1 5
+python data_curve.py L0 L0 5
 python data_curve.py L_ALL L_ALL 5
