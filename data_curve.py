498e208cad5abe418876e8e8bcfe8e334cddabf7
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2016-12-28 22:08:26 -0800

    Add neural classifier

--- data_curve.py
+++ data_curve.py
@@ -7,16 +7,18 @@ more data is added.
 """
 from collections import defaultdict
 from models.ibm2 import IBM2
+from models.neural_classifier import NNClassifier
 from preprocessor.reader import *
 from random import shuffle
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 import sys
 
 nl_format, ml_format, commands_format = "clean_data/test/%s.en", "clean_data/test/%s.ml", "clean_data/test/%s.commands"
 
 
-def loo_data_curve(nl_level, ml_level, save_id, step=20, save_fig=True):
+def loo_data_curve(nl_level, ml_level, save_id, model='ibm2', step=20, save_fig=True):
     """
     Performs LOO Cross-Validation, generates accuracy for the given Natural Language - Machine
     Language Pair.
@@ -34,9 +36,14 @@ def loo_data_curve(nl_level, ml_level, save_id, step=20, save_fig=True):
 
     chunk_sizes, accuracies = [], []
     for chunk_size in range(step, len(pc_train), step):
+        tf.reset_default_graph()
         dataset = list(pc_train[:chunk_size])
-        print 'Training IBM Model 2 on Chunk:', chunk_size
-        ibm2 = IBM2(dataset, 15)
+        print 'Training Model on Chunk:', chunk_size
+
+        if model == 'ibm2':
+            m = IBM2(dataset, 15)
+        elif model == 'neural':
+            m = NNClassifier(dataset, ml_commands)
 
         correct, total = 0, 0
         for i in range(len(pc_test) - 1):
@@ -44,11 +51,17 @@ def loo_data_curve(nl_level, ml_level, save_id, step=20, save_fig=True):
             example_en, example_ml = pc_test[i]
 
             # Score Translations
-            best_trans, best_score = None, 0.0
-            for t in ml_commands:
-                score = ibm2.score(example_en, t)
-                if score > best_score:
-                    best_trans, best_score = t, score
+            if model == 'ibm2':
+                best_trans, best_score = None, 0.0
+                for t in ml_commands:
+                    score = m.score(example_en, t)
+                    if score > best_score:
+                        best_trans, best_score = t, score
+
+            elif model == 'neural':
+                best_trans, best_score = m.score(example_en)
+
+            # Print Statistics
             print best_trans, best_score
 
             # Update Counters
@@ -82,11 +95,12 @@ if __name__ == "__main__":
     # Read Level-Combination from Command Line
     args = sys.argv
     en_lvl, ml_lvl = args[1], args[2]
+    model = args[3]
 
     # Run LOO Cross-Validation => Get Accuracy
     error_bar = defaultdict(list)
-    for i in xrange(int(args[3])):
-        x, y = loo_data_curve(en_lvl, ml_lvl, i + 1)
+    for i in xrange(int(args[4])):
+        x, y = loo_data_curve(en_lvl, ml_lvl, i + 1, model=model)
         for j in range(len(x)):
             error_bar[x[j]] += [y[j]]
 
@@ -94,7 +108,7 @@ if __name__ == "__main__":
     tuples = sorted(list(error_bar.iteritems()), key=lambda x: x[0])
     plt.plot([x[0] for x in tuples], [np.mean(x[1]) for x in tuples], 'g--')
     plt.errorbar([x[0] for x in tuples], [np.mean(x[1]) for x in tuples],
-                 yerr=1.96 * np.array([np.std(x[1]) for x in tuples]) * (1 / np.sqrt(int(args[3]))),
+                 yerr=1.96 * np.array([np.std(x[1]) for x in tuples]) * (1 / np.sqrt(int(args[4]))),
                  color='g')
     plt.title('%s - %s Test Accuracy vs. Number Training Examples' % (en_lvl, ml_lvl))
     plt.xlabel('Number of Training Examples')
--- models/neural_classifier.py
+++ models/neural_classifier.py
@@ -10,12 +10,17 @@ Architecture:
     - ReLU Layer
     - Softmax Output over Commands (Enumerated 1 - N)
 """
+import numpy as np
 import tensorflow as tf
-UNK = "<<UNK>>"
+import tflearn
+
+PAD, PAD_ID = "<<PAD>>", 0
+UNK, UNK_ID = "<<UNK>>", 1
 
 
 class NNClassifier():
-    def __init__(self, parallel_corpus, commands):
+    def __init__(self, parallel_corpus, commands, embedding_size=20, lstm_size=100, hidden_size=50,
+                 epochs=20, batch_size=32):
         """
         Instantiates and Trains Model using the given parallel corpus.
 
@@ -24,16 +29,29 @@ class NNClassifier():
                     2) List of target sentence tokens
         :param commands: List of Lists, where each element is one of the possible commands (labels)
         """
-        self.commands, self.labels = commands, {x: i for (i, x) in enumerate(commands)}
-        self.pc = parallel_corpus
+        self.commands, self.labels = commands, {" ".join(x): i for (i, x) in enumerate(commands)}
+        self.pc, self.epochs, self.bsz = parallel_corpus, epochs, batch_size
+        self.embedding_sz, self.lstm_sz, self.hidden_sz = embedding_size, lstm_size, hidden_size
 
         # Build Vocabulary
         self.word2id, self.id2word = self.build_vocabulary()
-        self.max_len, self.lengths = self.get_lengths()
+
+        # Vectorize Parallel Corpus
+        self.train_x, self.train_y = self.vectorize()
+        self.lengths = [len(n) for (n, _) in self.pc]
 
         # Setup Placeholders
-        self.X = tf.placeholder(tf.int32, shape=[None, self.max_len])
-        self.Y = tf.placeholder(tf.int32, shape=[None])
+        self.X = tf.placeholder(tf.int32, shape=[None, self.train_x.shape[-1]], name='NL_Command')
+        self.Y = tf.placeholder(tf.float32, shape=[None, len(self.commands)], name='ML_Command')
+        self.X_len = tf.placeholder(tf.int32, shape=[None], name='NL_Length')
+        self.keep_prob = tf.placeholder(tf.float32, shape=[None], name='Dropout_Prob')
+
+        # Build and Compile Model
+        self.model = self.compile_model()
+
+        # Fit Model
+        self.model.fit([self.train_x, self.lengths, [0.5] * len(self.lengths)], self.train_y,
+                       n_epoch=self.epochs, batch_size=self.bsz, snapshot_epoch=False)
 
     def build_vocabulary(self):
         """
@@ -41,24 +59,51 @@ class NNClassifier():
 
         :return: Tuple of Word2Id, Id2Word Dictionaries.
         """
-        vocab = {UNK}
+        vocab = set()
         for n, _ in self.pc:
             for word in n:
                 vocab.add(word)
 
-        id2word = list(vocab)
+        id2word = [PAD, UNK] + list(vocab)
         word2id = {id2word[i]: i for i in range(len(id2word))}
         return word2id, id2word
 
-    def get_lengths(self):
+    def vectorize(self):
         """
-        Read through the parallel corpus, get maximum lengths, and individual command lengths.
+        Step through the Parallel Corpus, and convert each sequence to vectors.
+        """
+        x, y = [], []
+        for nl, ml in self.pc:
+            nl_vec, ml_label = [self.word2id[w] for w in self.word2id], self.labels[" ".join(ml)]
+            x.append(nl_vec)
+            y.append(ml_label)
+        return tflearn.data_utils.pad_sequences(x), \
+            tflearn.data_utils.to_categorical(y, len(self.commands))
 
-        :return: Tuple of max_length, sequence lengths.
+    def compile_model(self):
         """
-        max_len, lengths = 0, []
-        for n, _ in self.pc:
-            if len(n) > max_len:
-                max_len = len(n)
-            lengths.append(len(n))
-        return max_len, lengths
+        Compile the LSTM Classifier, taking the input placeholder, generating the softmax
+        distribution over all possible reward functions.
+        """
+        x, xlen = tflearn.input_data(placeholder=self.X), tflearn.input_data(placeholder=self.X_len)
+        keep_prob = tflearn.input_data(placeholder=self.keep_prob)
+        embedding = tflearn.embedding(x, len(self.word2id), self.embedding_sz)
+        embedding = tflearn.dropout(embedding, keep_prob[0])
+        lstm = tflearn.lstm(embedding, self.lstm_sz, sequence_length=xlen)
+        hidden = tflearn.fully_connected(lstm, self.hidden_sz, activation='relu')
+        hidden = tflearn.dropout(hidden, keep_prob[0])
+        output = tflearn.fully_connected(hidden, len(self.commands), activation='softmax')
+        net = tflearn.regression(output, placeholder=self.Y)
+        return tflearn.DNN(net, tensorboard_dir='logs/')
+
+    def score(self, nl_command):
+        """
+        Given a natural language command, return predicted output and score.
+
+        :return: List of tokens representing predicted command, and score.
+        """
+        seq, seq_len = [self.word2id.get(w, UNK_ID) for w in nl_command], len(nl_command)
+        seq = tflearn.data_utils.pad_sequences([seq], maxlen=self.train_x.shape[1])
+        y = self.model.predict([seq, [seq_len], [1.0]])
+        pred_command = np.argmax(y, axis=1)
+        return self.commands[pred_command], y[0][pred_command]
