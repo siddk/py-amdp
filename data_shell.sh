2d0bf6a31aba4370009b73b3101bbcd5d71fe50e
Dilip <dilip.arumugam@gmail.com>
2016-12-29 12:48:55 -0800

    Updated scripts

--- data_shell.sh
+++ data_shell.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python data_curve.py L2 L2 10
-python data_curve.py L1 L1 10
-python data_curve.py L0 L0 10
-python data_curve.py L_ALL L_ALL 10
+python data_curve.py L2 L2 ibm2 10
+python data_curve.py L1 L1 ibm2 10
+python data_curve.py L0 L0 ibm2 10
+python data_curve.py L_ALL L_ALL ibm2 10
