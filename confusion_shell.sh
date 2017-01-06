3ea850d9f9218d436b13232d90d0aba7a0322f9c
Dilip <dilip.arumugam@gmail.com>
2017-01-05 21:00:44 -0800

    Added both neural models and updated shell script to run all neural and neural dual

--- confusion_shell.sh
+++ confusion_shell.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-python confusion_matrices.py single 10 L0
-python confusion_matrices.py single 10 L1
-python confusion_matrices.py single 10 L2
-python confusion_matrices.py all 10
+python confusion_matrices.py single rnn 10 L0
+python confusion_matrices.py single rnn 10 L1
+python confusion_matrices.py single rnn 10 L2
+python confusion_matrices.py all rnn 10
+python confusion_matrices.py single nn 10 L0
+python confusion_matrices.py single nn 10 L1
+python confusion_matrices.py single nn 10 L2
+python confusion_matrices.py all nn 10
 python neural_dual.py False 10
