b6a9f891e0702fbf1d69080ebe1c30069118561d
Dilip <dilip.arumugam@gmail.com>
2017-01-05 20:49:58 -0800

    Updated confusion matrix script

--- confusion_shell.sh
+++ confusion_shell.sh
@@ -4,3 +4,4 @@ python confusion_matrices.py single 10 L0
 python confusion_matrices.py single 10 L1
 python confusion_matrices.py single 10 L2
 python confusion_matrices.py all 10
+python dual_model.py False 10
